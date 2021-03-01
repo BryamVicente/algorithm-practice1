2355fb7e667e8655f205668321f2ba333a1fda3b
github username <vicenteBryam@gmail.com>
2021-03-01 17:03:02 -0500

    finished rotated sorted array

--- LeetCode/FindMinInRotatedSortedArray.js
+++ LeetCode/FindMinInRotatedSortedArray.js
@@ -21,7 +21,42 @@ Output: 0
 Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
 */
 
+/* When questions have to do with sorted data, it's best practice to approach problems using Binar 
+search method */
+
 const findMin = (nums) => {
 
-        
+    let left = 0
+    let right = nums.length - 1
+
+    if (nums.length === 1){
+        return nums[0]
+    }
+
+    if (nums[left] < nums[right]){
+        return nums[left]
+    }
+
+    while(left <= right){
+
+        const mid = Math.floor((left + right) / 2)
+
+        const leftVal = nums[left]
+        const midVal = nums[mid]
+        const leftOfMid = nums[mid - 1]
+        const rightOfMid = nums[mid + 1]
+
+        if (midVal > rightOfMid){
+            return rightOfMid
+        } else if (leftOfMid > midVal){
+            return midVal
+        }
+
+        if (midVal > leftVal){
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
 }
+console.log(findMin([3,4,5,1,2]))
