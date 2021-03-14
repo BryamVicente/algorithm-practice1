dc328e10adbc6b3a4aa91e8e4445c05ab030b137
github username <vicenteBryam@gmail.com>
2021-03-14 18:06:25 -0400

    cleaned up code

--- LeetCode/Random Problems/RichestCostumerWealth.js
+++ LeetCode/Random Problems/RichestCostumerWealth.js
@@ -1,19 +1,17 @@
-
 const maximumWealth = (accounts)=> {
-    let bigSum = 0
+    let richestCustomer = 0
     
     for(let i = 0; i < accounts.length; i++){
-       let currentSum = 0
+       let customerWealth = 0
        for( let j = 0; j < accounts[i].length; j++){
-        currentSum += accounts[i][j]
+        customerWealth += accounts[i][j]
        }
-        bigSum = Math.max(bigSum,currentSum)
+        richestCustomer = Math.max(richestCustomer,customerWealth)
     }
-
-    return bigSum
+    return richestCustomer
 };
 
-
- console.log(maximumWealth([[1,5],[7,3],[3,5]]))
+console.log(maximumWealth([[1,5],[7,3],[3,5]]))
 console.log(maximumWealth([[2,8,7],[7,1,3],[1,9,5]]))
 console.log(maximumWealth([[1,2,3],[3,2,1]]))
+
