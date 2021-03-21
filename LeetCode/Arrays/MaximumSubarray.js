cfa4125f94d39196791b364dee5e39b2b602c69a
github username <vicenteBryam@gmail.com>
2021-03-21 19:22:30 -0400

    finished blog for max subarray problem

--- LeetCode/Arrays/MaximumSubarray.js
+++ LeetCode/Arrays/MaximumSubarray.js
@@ -1,3 +1,25 @@
+const maxSubArray = (nums) => {
+
+    let largeSum = nums[0]
+
+    for (let i = 1; i < nums.length; i++){
+
+        nums[i] = Math.max(nums[i], nums[i] + nums[i-1])
+
+        largeSum = Math.max(largeSum, nums[i])
+    }   
+    return largeSum
+}
+
+console.log("First Exmaple: ",maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+console.log("Second Example: ",maxSubArray([1]))
+console.log("Third Example:  ",maxSubArray([5,4,-1,7,8]))
+
+
+    
+
+
+
 /*
 Given an integer array nums, find the contiguous subarray (containing at least one number) 
 which has the largest sum and return its sum.
@@ -13,22 +35,8 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 
 // create a variable that will be the largeSum 
 // largeSum should be equal to the beginning of the array
-// iterate over the nums array starting at 0
+// iterate over the nums array starting at 1 
     // we have to change nums[i] and use Math.max()
     // change the value of largeSum -> it has to include Math.max()
 // return the largeSum
-const maxSubArray = (nums) => {
 
-    let largeSum = nums[0]
- 
-
-    for (let i = 1; i < nums.length; i++){
-        nums[i] = Math.max(nums[i], nums[i] + nums[i-1])
-        
-        largeSum = Math.max(largeSum, nums[i])
-    }
-    return largeSum
-}
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
-console.log(maxSubArray([1]))
-console.log(maxSubArray([5,4,-1,7,8]))
