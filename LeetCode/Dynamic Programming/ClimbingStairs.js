b560627957519cc6e74c2358c46bee0064d0ecd1
github username <vicenteBryam@gmail.com>
2021-04-01 12:12:17 -0400

    finished

--- LeetCode/Dynamic Programming/ClimbingStairs.js
+++ LeetCode/Dynamic Programming/ClimbingStairs.js
@@ -16,4 +16,47 @@ Output: 3
 Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+input: n = 4
+
+1 + 1 + 1 + 1
+2 + 1 + 1 
+1 + 2 + 1
+1 + 1 + 2
+2 + 2 
+
+input: n = 5 
+1 + 1 + 1 + 1 + 1
+2 + 1 + 1 + 1 
+1 + 2 + 1 + 1 
+1 + 1 + 2 + 1 
+1 + 1 + 1 + 2
+2 + 2 + 1
+2 + 1 + 2
+1 + 2 + 2  
 */
+
+// const climbStairs = (n) => {
+
+//     let seen = {};
+
+//     if(n < 3){
+//         return n;
+//     }else if (!(n in seen)) {
+//         seen[n] = climbStairs(n-1) + climbStairs(n-2);
+//     }
+//     return seen[n];
+// };
+// console.log(climbStairs(3))
+const climbStairs = (n) => {
+
+    let a = 1, b = 2, next;
+        
+    for(let i = 3; i <= n; i++) {
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return n === 1 ? a : b;
+}
