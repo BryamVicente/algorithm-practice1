473b9f21d56aac3424adf8dfb41059f5b7349265
github username <vicenteBryam@gmail.com>
2021-04-23 14:20:07 -0400

    first edit for blog

--- Udemy/Fib.js
+++ Udemy/Fib.js
@@ -1,12 +1,13 @@
 // Recursive solution!!!
-// const fib = (n) => {
-//     if (n <= 2){
-//         return 1
-//     }
-//     return fib(n-1) + fib(n-2)
-// }
 
-// console.log(fib(40))
+const fib = (n) => {
+    if (n <= 2){
+        return 1
+    }
+    return fib(n-1) + fib(n-2)
+}
+
+console.log(fib(40))
 
 
 // Memoization Solution!!!
@@ -28,15 +29,15 @@
 
  
 // Tabulated Version 
-const fib = (n) => {
-    if (n <= 2){
-        return 1
-    }
-    let fibNums = [undefined, 1, 1]
-    for (let i =3; i <= n; i++){
-        fibNums[i] = fibNums[i-1] + fibNums[i-2]
-    }
-    return fibNums[n]
-}
+// const fib = (n) => {
+//     if (n <= 2){
+//         return 1
+//     }
+//     let fibNums = [undefined, 1, 1]
+//     for (let i =3; i <= n; i++){
+//         fibNums[i] = fibNums[i-1] + fibNums[i-2]
+//     }
+//     return fibNums[n]
+// }
 
-console.log(fib(4))
+// console.log(fib(4))
