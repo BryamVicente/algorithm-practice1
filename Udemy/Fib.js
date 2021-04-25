114b1b010d90e3cab553004f458a3e0e281bfce2
github username <vicenteBryam@gmail.com>
2021-04-25 16:24:31 -0400

    third edit

--- Udemy/Fib.js
+++ Udemy/Fib.js
@@ -12,24 +12,24 @@
 
 // Memoization Solution!!!
 
-const fib = (n) => {
+// const fib = (n) => {
 
-    let memo =[]
+//     let memo =[]
 
-    if (memo[n] !== undefined){
-        return memo[n]
-    }
-    if (n <= 2){
-        return 1
-    }
+//     if (memo[n] !== undefined){
+//         return memo[n]
+//     }
+//     if (n <= 2){
+//         return 1
+//     }
 
-    let res = fib(n-1, memo) + fib(n-2, memo)
+//     let res = fib(n-1, memo) + fib(n-2, memo)
 
-    memo[n] = res
-    return res
-}
+//     memo[n] = res
+//     return res
+// }
 
-console.log(fib(4))
+// console.log(fib(4))
 
 
 // still memoization
@@ -48,16 +48,18 @@ console.log(fib(4))
 //     return res
 // }
  
-// Tabulated Version 
-// const fib = (n) => {
-//     if (n <= 2){
-//         return 1
-//     }
-//     let fibNums = [undefined, 1, 1]
-//     for (let i =3; i <= n; i++){
-//         fibNums[i] = fibNums[i-1] + fibNums[i-2]
-//     }
-//     return fibNums[n]
-// }
 
-// console.log(fib(4))
+// Tabulated Version 
+
+const fib = (n) => {
+    if (n <= 2){
+        return 1
+    }
+    let fibNums = [undefined, 1, 1]
+    for (let i =3; i <= n; i++){
+        fibNums[i] = fibNums[i-1] + fibNums[i-2]
+    }
+    return fibNums[n]
+}
+
+console.log(fib(4))
