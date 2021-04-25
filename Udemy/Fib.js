2c685486ab82b36130a786db8d2d6b93033a83d8
github username <vicenteBryam@gmail.com>
2021-04-25 17:23:08 -0400

    fifth edit

--- Udemy/Fib.js
+++ Udemy/Fib.js
@@ -52,16 +52,17 @@
 // Tabulated Version 
 
 const fib = (n) => {
+
     if (n <= 2){
         return 1
     }
 
-    let fibNums = [undefined, 1, 1]
-    
+    let fibNumbers = [undefined, 1, 1]
+
     for (let i =3; i <= n; i++){
-        fibNums[i] = fibNums[i-1] + fibNums[i-2]
+        fibNumbers[i] = fibNumbers[i-1] + fibNumbers[i-2]
     }
-    return fibNums[n]
+    return fibNumbers[n]
 }
 
 console.log(fib(4))
