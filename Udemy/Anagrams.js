a6585c7520cea84280cc7b94aa0d1d24356f1176
github username <vicenteBryam@gmail.com>
2021-04-30 10:29:59 -0400

    added instructions

---
+++ Udemy/Anagrams.js
@@ -0,0 +1,12 @@
+/*
+Check to see if two provided strings ate anagrams of each other. 
+One string is an anagram of another if it uses the same characters in the same quantity. 
+Only consider chatacters, not spaces or punctuation. 
+Consider capital letters to be the same as lower case!
+
+--- Examples
+    anagrams('rail safety', 'fairy tales') --> True 
+    anagrams('RAIL! SAFETY','fairy tales') --> True
+    anagrams('Hi there','Bye there') --> False
+
+*/
