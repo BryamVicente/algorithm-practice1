67db41fa7d2132c24cd84b057e669c4e63963708
github username <vicenteBryam@gmail.com>
2021-04-30 11:50:41 -0400

    Making a lot of porgress

--- Udemy/Capitalization.js
+++ Udemy/Capitalization.js
@@ -11,3 +11,24 @@ Write a function that accepts a string. The function should capitalize the first
 // if the word is "there"
     // word[0].toUpperCase --> T
     // word.slice(1) ---> here
+
+const capitalize = (string) => {
+    // Create empty array
+    let words = []
+    
+    //split the input 'string'
+    let splitString = string.split(' ')
+
+    // create for loop to iterate 
+    for (let word of splitString){
+        // Upper case the first letter of the word 
+        // Join first letter with rest of the string
+        // Push result inside empty array
+
+
+    }
+    
+    // Join the 'empty string' into a string and return 
+
+}
+console.log(capitalize('a short sentence'))
