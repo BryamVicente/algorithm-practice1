5f6a5573dc0a773f262b4569267184f8b22cd115
github username <vicenteBryam@gmail.com>
2021-04-30 11:43:31 -0400

    Created a file for Capitalization

---
+++ Udemy/Capitalization.js
@@ -0,0 +1,13 @@
+/*
+Write a function that accepts a string. The function should capitalize the first letter of each word in the string then return the capitalized string.
+
+-- Examples:  
+    capitalized('a short sentence') --> "A Short Sentence"
+    capitalized('a lazy fox') --> "A Lazy Fox"
+    capitalized('look, it is working!') --> "Look, It Is Working!"
+
+*/
+
+// if the word is "there"
+    // word[0].toUpperCase --> T
+    // word.slice(1) ---> here
