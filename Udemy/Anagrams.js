9cb346875571e9642a653c8b9ad7ad128cf640fc
github username <vicenteBryam@gmail.com>
2021-04-30 11:00:24 -0400

    making progress with anagrams

--- Udemy/Anagrams.js
+++ Udemy/Anagrams.js
@@ -1,8 +1,11 @@
 /*
 Check to see if two provided strings ate anagrams of each other. 
 One string is an anagram of another if it uses the same characters in the same quantity. 
-Only consider chatacters, not spaces or punctuation. 
-Consider capital letters to be the same as lower case!
+    // For these two always do: 
+        Only consider chatacters, not spaces or punctuation. 
+        Consider capital letters to be the same as lower case!
+
+       --->  string.replace(/[^\w]/g, "").toLowerCase()
 
 --- Examples:
 
@@ -11,9 +14,29 @@ Consider capital letters to be the same as lower case!
     anagrams('Hi there','Bye there') --> False
 */
 
+// We can build a hashtable to solve this problem 
+
+// This is a helper function that creates a hash table 
+const helper = (string) => {
+    let charMap = {}
+    
+    for (let char of string.replace(/[^\w]/g, '').toLowerCase()){
+        charMap[char] = charMap[char] + 1 || 1
+    }
+    return charMap
+}
+
 const anagrams = (first, second) => {
 
-    
+   const firstCharMap = helper(first)
+   const secondCharMap = helper(second)
+
+//    console.log("first",firstCharMap)
+//    console.log("second",secondCharMap)
+
+    if (Object.keys(firstCharMap).length !== Object.keys(secondCharMap).length){
+        
+    }
 
 }
 console.log(anagrams('rail safety', 'fairy tales'))
