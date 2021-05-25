8367ab57ab34c6a65c3b197c5d2f98ff6408a54d
github username <vicenteBryam@gmail.com>
2021-05-24 21:20:32 -0400

    almost done

--- LeetCode/Random Problems/RunningSumof1dArray.js
+++ LeetCode/Random Problems/RunningSumof1dArray.js
@@ -2,16 +2,15 @@
 const runningSum = (nums) => {
 
     for (let i = 1; i < nums.length; i++){
-        console.log("this is nums[i]", nums[i])
+        nums[i] = nums[i] + nums[i -1]
     }
-
+    return nums
 }
 console.log(runningSum([1,2,3,4]))
+console.log(runningSum([1,1,1,1,1]))
+console.log(runningSum([3,1,2,10,1]))
 
 
-
-// console.log(runningSum([1,1,1,1,1]))
-// console.log(runningSum([3,1,2,10,1]))
 
 // for (let i = 1; i < nums.length; i++){
 //     nums[i] = nums[i] + nums[i -1]
