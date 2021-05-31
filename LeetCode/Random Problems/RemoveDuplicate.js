41219c4e0f4f3844b11d0692f35848eb15a8918b
github username <vicenteBryam@gmail.com>
2021-05-31 18:42:59 -0400

    completed for loop

--- LeetCode/Random Problems/RemoveDuplicate.js
+++ LeetCode/Random Problems/RemoveDuplicate.js
@@ -2,11 +2,16 @@
 const removeDuplicates = (nums) => {
 
     // Create a variable that represents the length of the array
+    let arrayLength = 1 
 
     // Iterate over the 'nums' array using a for statement 
+    for (let i = 1; i < nums.length; i++){
         // Create a conditional that compares the current number 'nums[i]' to the previous
-        // If they're not equal to each other, then incremenet the length of the array
-    //
+        if (nums[i] != nums[i-1]){
+            // If they're not equal to each other, then incremenet the length of the array
+            nums[arrayLength++] = nums[i]  
+        }
+    }
 
     // Create a conditional that returns the length of the nums array if the length is less than or equal to 1
 
