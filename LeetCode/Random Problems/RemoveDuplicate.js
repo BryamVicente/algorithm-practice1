be7e3834be3035c2186c53d8242cb1e4b1dc9bdc
github username <vicenteBryam@gmail.com>
2021-05-31 21:03:50 -0400

    complete

--- LeetCode/Random Problems/RemoveDuplicate.js
+++ LeetCode/Random Problems/RemoveDuplicate.js
@@ -12,37 +12,14 @@ const removeDuplicates = (nums) => {
             nums[arrayLength++] = nums[i]  
         }
     }
-
     // Create a conditional that returns the length of the nums array if the length is less than or equal to 1
-
+    if (nums.length <= 1){
+        return nums.length
+    }
     //return the length of the array
+    return arrayLength
 }
-console.log(removeDuplicates([1,1,2]))
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+console.log('Example 1: ',removeDuplicates([1,1,2]))
+console.log('Example 2: ',removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
 
-// const removeDuplicates = (nums) => {
 
-//     // BIG QUESTION: CAN I USE SETS FOR THIS PROBLEM??
-
-//     // Create a conditional that returns the length of nums if the length is less that 1 
-//     if (nums.length <= 1){
-//         return nums.length
-//     }
-    
-//     // Create a variable represents the length of the array; start at 1
-//     let arrayLength = 1
-
-//     // Iterate over the sorted nums 
-//     for (let i = 1; i < nums.length; i++){
-//         // Conditionat states --> if the current element being iterated is not equal to the previous
-//         if (nums[i] != nums[i-1]){
-
-//             // increment Arraylength 
-//             nums[arrayLength++] = nums[i]  
-//         }
-//     }
-//     //return the length of the "new array" which is an integer 
-//     return arrayLength
-// };
-// console.log(removeDuplicates([1,1,2]))
-// console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
