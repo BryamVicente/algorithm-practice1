514b108fe06a2681984f08ccd93d52e9051ad06b
github username <vicenteBryam@gmail.com>
2021-06-07 15:10:28 -0400

    almost done

--- LeetCode/Random Problems/groupAnagrams.js
+++ LeetCode/Random Problems/groupAnagrams.js
@@ -1,28 +1,24 @@
 
 
 const groupAnagrams = (strs) => {
-
     // Create hash table to keep track of which strings are made with what letters 
     const hashTable = {}
     
     // Create for loop to itereate over "strs"
     for (let i = 0; i < strs.length; i++){
     // While looping through "strs", we need to sort the strings alphabetically 
+    let sortedString = strs[i].split("").sort().join("")
     
     // Create conditional: If the sorted string doesn't exist in the hash, add it as a key! 
-    // push the unsorted string into the key's existing array of values 
-    // console.log("sorted string: ", hashTable)
+        if (!hashTable[sortedString]){
+            hashTable[sortedString] = [strs[i]]
+        }else {
+            // push the unsorted string into the key's existing array of values 
+            hashTable[sortedString].push(strs[i])
+        }
     }
-    
     // Since the question is asking to return array of the anagrams, we have to convert from Object to Array
+    return Object.values(hashTable)
 }
 console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 
-
-    // let sortedString = strs[i].split("").sort().join("")
-    // if (!hashTable[sortedString]){
-        //     hashTable[sortedString] = [strs[i]]
-        // }else {
-            //     hashTable[sortedString].push(strs[i])
-            // }
-            // return Object.values(hashTable)
