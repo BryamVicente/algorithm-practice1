3137e08520e3ed746475e9266664a9492a4f01c9
github username <vicenteBryam@gmail.com>
2021-06-14 13:21:40 -0400

    just started

--- LeetCode/Random Problems/SumOfUniqueElements.js
+++ LeetCode/Random Problems/SumOfUniqueElements.js
@@ -1,9 +1,31 @@
 
 const sumOfUnique = (nums) => {
+    // Create hash table to keep count of unique elements
     let obj = {}
-    let sum = 0
-    
-    for(let num of nums){
+    // The expected output is an integer which means we need a variable that represents the sum
+    let sum = 0 
+  
+    //Create a for of loop 
+        // Create conditional -> if the current number exist inside the hash table, then incremnt by 1
+        // else make that current number equal to 1 
+    //
+
+    // Create a for in loop for hash table
+        // Create conditional --> if the key value is less than 2 increment sum value 
+    //
+
+    //return the sum 
+
+};
+console.log(sumOfUnique([1,2,3,2]))
+
+
+
+
+/*
+  
+
+for(let num of nums){
         if(obj[num] > 0){
             obj[num]++
         } else {
@@ -17,5 +39,5 @@ const sumOfUnique = (nums) => {
         }
     }
     return sum
-};
-console.log(sumOfUnique([1,2,3,2]))
+
+*/
