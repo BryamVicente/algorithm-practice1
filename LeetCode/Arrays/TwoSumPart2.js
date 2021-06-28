6ce80cedcb9c1dc8ecdf0777dd20a617070cb451
github username <vicenteBryam@gmail.com>
2021-06-28 14:39:48 -0400

    created function and one testing case

--- LeetCode/Arrays/TwoSumPart2.js
+++ LeetCode/Arrays/TwoSumPart2.js
@@ -22,19 +22,9 @@ Output: [1,2]
 */
 
 
-/*
 const twoSum = (numbers, target) => {
-    let p1 = 0
-    let p2 = numbers.length - 1
     
-    while (numbers[p1] + numbers[p2] !== target) {
-        if (numbers[p1] + numbers[p2] > target) {
-            p2--
-        } else {
-            p1++
-        }
-    }
     
-    return [p1 + 1, p2 + 1]
+    
 }
-*/
+console.log(twoSum([2,7,11,15], 9))
