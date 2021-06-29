12484f784748194a9d85e19cc768130d70349ef8
github username <vicenteBryam@gmail.com>
2021-06-28 22:57:42 -0400

    almost done

--- LeetCode/Arrays/TwoSumPart2.js
+++ LeetCode/Arrays/TwoSumPart2.js
@@ -3,18 +3,22 @@ const twoSum = (numbers, target) => {
     let left = 0, right = numbers.length-1, theSum;
 
     while (left < right) {
+        theSum = numbers[left] + numbers[right];
 
+        if (theSum == target){
+            return [left+1,right+1];
+        } else if (theSum > target){
+            right--;
+        } else {
+            left++
+        }
     }
 }
-console.log(twoSum([2,7,11,15], 17))
+console.log("Example 1: ",twoSum([2,7,11,15], 17))
 
 
 
 
-// while (l < r) {
-//     sum = numbers[l] + numbers[r];
-//     if (sum == target) return [l+1,r+1];
-//     else if (sum > target) r--;
-//     else l++;
-// }
+
+
 
