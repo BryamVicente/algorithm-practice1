638dcdc7d97557662a3e364ce6f32be9d691df47
github username <vicenteBryam@gmail.com>
2021-06-28 22:30:56 -0400

    created all essential variables

--- LeetCode/Arrays/TwoSumPart2.js
+++ LeetCode/Arrays/TwoSumPart2.js
@@ -1,8 +1,16 @@
 
 const twoSum = (numbers, target) => {
-    
-  
+    let left = 0, right = numbers.length-1, theSum;
 }
 console.log(twoSum([2,7,11,15], 17))
 
+
+
+
+// while (l < r) {
+//     sum = numbers[l] + numbers[r];
+//     if (sum == target) return [l+1,r+1];
+//     else if (sum > target) r--;
+//     else l++;
+// }
 
