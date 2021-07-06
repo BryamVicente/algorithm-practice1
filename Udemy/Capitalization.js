203c5b9cc0e23c02be8046bfd0df2cb6a894f456
github username <vicenteBryam@gmail.com>
2021-07-05 22:36:20 -0400

    done

--- Udemy/Capitalization.js
+++ Udemy/Capitalization.js
@@ -1,5 +1,6 @@
 /*
-Write a function that accepts a string. The function should capitalize the first letter of each word in the string then return the capitalized string.
+Write a function that accepts a string. The function should capitalize the first letter 
+of each word in the string then return the capitalized string.
 
 -- Examples:  
     capitalized('a short sentence') --> "A Short Sentence"
@@ -21,10 +22,13 @@ const capitalize = (string) => {
 
     // create for loop to iterate 
     for (let word of splitString){
-    // Upper case the first letter of the word, Join(+) first letter with rest of the string and Push result inside empty array
+    // Upper case the first letter of the word, Join(+) first letter with rest of the string 
+    //and Push result inside empty array
         words.push(word[0].toUpperCase() + word.slice(1))
     }
     // Join the 'empty string' into a string and return 
    return words.join( ' ')
 }
 console.log(capitalize('a short sentence'))
+
+
