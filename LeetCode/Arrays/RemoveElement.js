e36f9c2fba2b41834e5522f5b6c0c26d7127a025
github username <vicenteBryam@gmail.com>
2021-07-12 21:18:56 -0400

    created if statement logic

--- LeetCode/Arrays/RemoveElement.js
+++ LeetCode/Arrays/RemoveElement.js
@@ -3,8 +3,12 @@ const removeElement = (nums, val) => {
 
     //Create a for loop to iterate over nums 
     for (let i = 0; i < nums.length; i++) {
-        // Create a for loop that can compare the current number and val
+        // Create an if statement that can compare the current number and val
+        if(nums[i] === val){
         // if they match I should replace/swap them
+
+        }
+       
         // if they don't then i should decrement
     }
 
