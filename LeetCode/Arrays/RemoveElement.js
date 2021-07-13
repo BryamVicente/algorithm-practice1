a5fa5c75a99925443c45e8ffe759e97e4a08ffd2
github username <vicenteBryam@gmail.com>
2021-07-12 20:51:19 -0400

    edited a pseudo code

--- LeetCode/Arrays/RemoveElement.js
+++ LeetCode/Arrays/RemoveElement.js
@@ -1,1 +1,12 @@
 
+const removeElement = (nums, val) => {
+
+    //Create a for loop to iterate over nums 
+        // Create a for loop that can compare the current number and val
+        // if they match I should replace/swap them
+        // if they don't then i should decrement
+    //
+
+    // return the nums length 
+}
+console.log("Example 1: ", removeElement([3,2,2,3], 3))
