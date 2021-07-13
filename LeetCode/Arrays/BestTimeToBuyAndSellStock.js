b778deaa313c52bcdb56d9cf7811639175cb2c29
github username <vicenteBryam@gmail.com>
2021-07-13 16:14:53 -0400

    Finished BestTimeToBuyAndSellStock

--- LeetCode/Arrays/BestTimeToBuyAndSellStock.js
+++ LeetCode/Arrays/BestTimeToBuyAndSellStock.js
@@ -16,14 +16,20 @@ Note that buying on day 2 and selling on day 1 is not allowed because you must b
 
 const maxProfit = (prices) => {
     // Create a variable that represents the max number 
+    let maxNumber = 0
     // prices[0] is the beginning of the array. which means that I can't sell on this day 
+    let firstDay = prices[0]
 
     //for loop that iterates over prices. I'm starting the 1th place since it represents the first day!  
+    for(let i = 1; i < prices.length; i++){
+
         // Compare prices[i] and prices[0] to see which is the smaller number 
+        firstDay = Math.min(firstDay,prices[i])
         // Compare the max number and prices[i] - the beginning of array and see which number is higher  
-
+        maxNumber = Math.max(maxNumber, prices[i]-firstDay)
+        console.log("Just testing: ",maxNumber)
+    }
     // Return the max number/profit 
-
 }
 console.log("Example 1: ", maxProfit([7,1,5,3,6,4]))
 
