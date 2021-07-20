9fc3393ffef1898a1cabfcd35473e5e3118745c3
github username <vicenteBryam@gmail.com>
2021-07-19 22:26:40 -0400

    finished pseudo code

--- Udemy/Anagrams.js
+++ Udemy/Anagrams.js
@@ -1,11 +1,17 @@
 
-// First I will create a helper function that will iterate over a string 
+// First I will create a helper function that takes in a string as an argument
     // I'll create a hashtable to keep track of the characters in a string 
     // Itereate over the string
     // return that specific string 
 //
 
 
-// Then create another function that would be the main function 
+// Create the main function where I pass in string1 and string2
     // Call the helper function that I created and use it on both strings 
-    // 
+    // Create a conditional that compares the length of both strings being compared
+        // return false if they don't match 
+    // Iterate over first string 
+        // Create a conditonal that compares the current character of both string1 and string2 
+            // return false if they don't match 
+    // return true if they match 
+
