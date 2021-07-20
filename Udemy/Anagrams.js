efbec9c596df381e958c3dea3ece57eb10b911e5
github username <vicenteBryam@gmail.com>
2021-07-19 22:37:22 -0400

    finished helper function

--- Udemy/Anagrams.js
+++ Udemy/Anagrams.js
@@ -4,10 +4,12 @@ const helperFunction = (string) => {
     // I'll create a hashtable to keep track of the characters in a string 
     let hashString = {}
     // Itereate over the string
+    for(let character of string.replace(/[^/w]/g,"").toLowerCase()){
+        hashString[character] = hashString[character] + 1 || 1
+    }
     // return that specific string 
+    return hashString
 }
-
-
 // Create the main function where I pass in string1 and string2
     // Call the helper function that I created and use it on both strings 
     // Create a conditional that compares the length of both strings being compared
