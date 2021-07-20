629795362e826b223c23cae7d9813808dd6bc6b6
github username <vicenteBryam@gmail.com>
2021-07-19 22:07:13 -0400

    started

--- Udemy/Anagrams.js
+++ Udemy/Anagrams.js
@@ -1,69 +1,7 @@
-/*
-Check to see if two provided strings ate anagrams of each other. 
-One string is an anagram of another if it uses the same characters in the same quantity. 
-    // For these two always do: 
-        Only consider chatacters, not spaces or punctuation. 
-        Consider capital letters to be the same as lower case!
 
-       --->  string.replace(/[^\w]/g, "").toLowerCase()
-
---- Examples:
-
-    anagrams('rail safety', 'fairy tales') --> True 
-    anagrams('RAIL! SAFETY','fairy tales') --> True
-    anagrams('Hi there','Bye there') --> False
-*/
-
-//helper function
-const cleanStringHelper = (string) => {
-    // return string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
-    console.log("testing", string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join(''))
-}
-
-const anagrams = (first, second) => {
-   return cleanStringHelper(first) === cleanStringHelper(second)
-}
-
-console.log(anagrams('rail safety', 'faiiry tales'))
-// console.log(anagrams('Hi there','Bye there'))
+// First I will create a helper function that will iterate over a string 
+    // 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// This is a helper function that creates a hash table 
-// const helper = (string) => {
-//     let charMap = {}
-    
-//     for (let char of string.replace(/[^\w]/g, '').toLowerCase()){
-//         charMap[char] = charMap[char] + 1 || 1
-//     }
-//     return charMap
-// }
-
-// const anagrams = (first, second) => {
-
-//    const firstCharMap = helper(first)
-//    const secondCharMap = helper(second)
-
-//     if (Object.keys(firstCharMap).length !== Object.keys(secondCharMap).length){
-//         return false 
-//     }
-    
-//     for (let char in firstCharMap){
-//         if (firstCharMap[char] !== secondCharMap[char]){
-//             return false 
-//         }
-//     }
-//     return true 
-// }
-// console.log(anagrams('rail safety', 'fairy tales'))
+// Then create another function that would be the main function 
