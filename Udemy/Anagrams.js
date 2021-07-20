cad98f6c1976c6e8025a15ceba4bb7c2aa426696
github username <vicenteBryam@gmail.com>
2021-07-19 22:21:03 -0400

    started pseudo code for first function

--- Udemy/Anagrams.js
+++ Udemy/Anagrams.js
@@ -1,7 +1,11 @@
 
 // First I will create a helper function that will iterate over a string 
-    // 
-
+    // I'll create a hashtable to keep track of the characters in a string 
+    // Itereate over the string
+    // return that specific string 
+//
 
 
 // Then create another function that would be the main function 
+    // Call the helper function that I created and use it on both strings 
+    // 
